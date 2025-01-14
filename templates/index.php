0a674fbaa081403cf2e5470f88b0fc35a6fc8a65
provokateurin <kate@provokateurin.de>
2025-01-14 09:28:43 +0100

    fix(templates): Fix loading script and style
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 use OCP\Util;
 
-Util::addScript(OCA\AppTemplate\AppInfo\Application::APP_ID, 'main');
+Util::addScript(OCA\AppTemplate\AppInfo\Application::APP_ID, OCA\AppTemplate\AppInfo\Application::APP_ID . '-main');
+Util::addStyle(OCA\AppTemplate\AppInfo\Application::APP_ID, OCA\AppTemplate\AppInfo\Application::APP_ID . '-main');
 
 ?>
 
